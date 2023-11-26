df8489b89c2778fd1b9e305bd750f47039ae5415
Ashish032 <studu217@gmail.com>
2023-11-26 08:37:23 +0530

    .

--- lib/details_Screen.dart
+++ lib/details_Screen.dart
@@ -15,48 +15,50 @@ class MovieDetailsScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.network(
-              movie['image'] != null ? movie['image']['original'] : '',
-              height: 400.0,
-              width: double.infinity,
-              fit: BoxFit.fill,
-            ),
-            const SizedBox(height: 16.0),
-            Text(
-              movie['name'] ?? 'N/A',
-              style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0,
-                  color: Colors.white),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(245, 230, 230, 235),
-                  minimumSize: const Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      side: const BorderSide(
-                          color: Color.fromARGB(245, 158, 158, 168)))),
-              onPressed: () {},
-              child: const Text(
-                'Play',
-                style: TextStyle(fontSize: 17, color: Colors.black),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                movie['image'] != null ? movie['image']['original'] : '',
+                height: 400.0,
+                width: double.infinity,
+                fit: BoxFit.fill,
               ),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              movie['summary'] != null
-                  ? movie['summary'].replaceAll(RegExp(r'<[^>]*>'), '')
-                  : 'No summary available',
-              style: const TextStyle(fontSize: 16.0, color: Colors.white),
-            ),
-          ],
+              const SizedBox(height: 16.0),
+              Text(
+                movie['name'] ?? 'N/A',
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                    color: Colors.white),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(245, 230, 230, 235),
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        side: const BorderSide(
+                            color: Color.fromARGB(245, 158, 158, 168)))),
+                onPressed: () {},
+                child: const Text(
+                  'Play',
+                  style: TextStyle(fontSize: 17, color: Colors.black),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                movie['summary'] != null
+                    ? movie['summary'].replaceAll(RegExp(r'<[^>]*>'), '')
+                    : 'No summary available',
+                style: const TextStyle(fontSize: 16.0, color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     );
